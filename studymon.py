a666c8c9b0c03309ae5b21125e2b282116510860
CalvinaHarr <calvinaharr@hotmail.com>
2024-09-14 14:45:19 -0400

    trying to fix evolution stuff

--- studymon.py
+++ studymon.py
@@ -2,33 +2,22 @@
 import requests
 import json
 class Studymon():
-    level = 0
-    api_info = ""
-    next_stage = None
-    sprite = None
     def __init__(self, n, l):
         #attribute
         self.name = n
         self.level = l
         #getting stuff from pokeapi url
         base_url = "https://pokeapi.co/api/v2/"
-        req_gen = requests.get(base_url + "pokemon/" + n)
-        general_info = json.loads(req_gen.text)
-        self.base_experience = general_info.get("base_experience")
+        self.data = json.loads(requests.get(base_url + "pokemon/" + n))
+        # general_info = json.loads(self.data)
         
         self.evo_chain = self.fetch_evolution_chain()
 
-        #determines whether a studymon is a part of an evolutionary chain
-        if(len(self.evo_chain.get("evolution_details")) > 0):
-            self.can_evolve() = True
-        #gets the level the mon needs to be to evolve as well as the name of its evolution
-        if(self.can_evolve()):
-            self.evolution_level = self.evo_chain.get("evolves_to").get("evolution_details")[0].get("min_level")
-            self.next_stage = self.evo_chain.get("evolves_to").get("species").get("name")
+        #TODO get the level the mon needs to be to evolve as well as the name of its evolution
+
         #sprite stuff
         self.sprite = general_info.get("sprites").get("front_default")
-    
-    
+
     #Determines whether a studymon can evolve
     def can_evolve(self) -> bool:
         current_species = self.evo_chain['chain']
@@ -41,16 +30,25 @@ class Studymon():
     
     def fetch_evolution_chain(self):
         species_url = self.data['species']['url']
-        species_data = requests.get(species_url).json()
+        species_data = json.loads(requests.get(species_url).json())
         evolution_url = species_data['evolution_chain']['url']
         return json.loads(requests.get(evolution_url).json())
     
     #Turns this studymon into a new studymon
     def evolve(self):
-        if(not self.can_evolve()):
+        if not self.can_evolve():
+            print(f"{self.name} cannot evolve further.")
             return
-        else:
-            self = Studymon(self.next_stage)
+
+        current_species = self.evolution_chain['chain']
+        while current_species['species']['name'] != self.name:
+            current_species = current_species['evolves_to'][0]
+        
+        evolved_species = current_species['evolves_to'][0]['species']['name']
+        evolved_pokemon = Studymon(evolved_species)
+        
+        print(f"{self.name} evolved into {evolved_pokemon.name}!")
+        self.__dict__.update(evolved_pokemon.__dict__)
 
     #runs the exp calculator to earn exp
     
@@ -61,3 +59,7 @@ class Studymon():
     
     def getSprite(self) -> str:
         return self.sprite
+
+s = Studymon("bulbasaur", 1)
+print(s.can_evolve)
+print("\ntrue <-expected")
