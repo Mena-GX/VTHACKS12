0e9f8b4d74d798968345fee32e190a58db7159e4
Zorn-sketcher <thihanlt@gmail.com>
2024-09-14 10:53:57 -0400

    cd

--- studymonclass.py
+++ studymonclass.py
@@ -2,54 +2,16 @@
 class Studymon():
     level = 0
     name = "mon"
+    experience = 0
     modelID = 1
-    def __init__(self, l, n, m):
+    def __init__(self, l, n, m, e):
         self.level = l
         self.name = n
         self.modelID = m
+        self.experience = e
     def sayhi():
         print("hello")
-    
-# fire type studymon class
-class Firemon(Studymon):
-    type = "Fire"
-    experience = 0
-    def __init__(self, l, n, m, e):
-        super().__init__(l, n, m)
-        self.experience = e
-    def ShowFire():
-        print("")
 
-# water type studymon class
-class Watermon(Studymon):
-    type = "Water"
-    experience = 0
-    def __init__(self, l, n, m, e):
-        super().__init__(l, n, m)
-        self.experience = e
-    def LiftWater():
-        print("")
-
-# Earth type studymon class 
-class Earthmon(Studymon):
-    type = "Earth"
-    experience = 0
-    def __init__(self, l, n, m, e):
-        super().__init__(l, n, m)
-        self.experience = e
-    def LiftEarth():
-        print("")
-
-# Air type studymon class
-class Airmon(Studymon):
-    type = "Air"
-    experience = 0
-    def __init__(self, l, n, m, e):
-        super().__init__(l, n, m)
-        self.experience = e
-    def LiftSelf():
-        print("")
-    
     
     
 #class SubClass(SuperClass):
