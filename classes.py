10fb373e51919a441a490d79ba77a05702767048
Zorn-sketcher <thihanlt@gmail.com>
2024-09-14 08:44:08 -0400

    bob

--- classes.py
+++ classes.py
@@ -3,11 +3,14 @@
 class Student:
     def hello():
         print("print")
-
+    level = 1
+# studymon objects
 class Studymon:
     def hello():
         print("print")
-
-class reward(Studymon):
+# rewarded studymons
+class Reward(Studymon):
     def hello():
         print("prinerg")
+
+Bob = Student()
