42ac0678375a09a5ee207851c02c363d39abf710
Zorn-sketcher <thihanlt@gmail.com>
2024-09-14 08:36:57 -0400

    hello

---
+++ classes.py
@@ -0,0 +1,13 @@
+
+# student names
+class Student:
+    def hello():
+        print("print")
+
+class Studymon:
+    def hello():
+        print("print")
+
+class reward(Studymon):
+    def hello():
+        print("prinerg")
