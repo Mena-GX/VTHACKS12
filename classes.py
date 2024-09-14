91edf69eadd51af9493bce61d81da90203ef3540
Zorn-sketcher <thihanlt@gmail.com>
2024-09-14 15:02:04 -0400

    organize

--- classes.py
+++ classes.py
@@ -7,22 +7,8 @@ class Teacher:
     def __init__(self, tu, tp):
         self.t_username = tu
         self.t_password = tp
-    section = []
-
-# student names
-class StudentAccount:
-    username = None
-    password = None
-    def __init__(self, u, p):
-        self.username = u
-        self.password = p
-        
     
-class Student(StudentAccount):
-    full_name = None
-    def __init__():
-        super().__init__(u, p)
-    level = 1
+
 
 #Bob = input("Enter username:")
 #print("Username is: " + username)
